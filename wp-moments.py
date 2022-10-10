6679846f544eb4fe9d17887f1df5a1485843ce4c
Mr W-P <twilliam-powlett@birchwoodhigh.org.uk>
2022-10-10 13:49:00 +0100

    initial moment

---
+++ wp-moments.py
@@ -0,0 +1,5 @@
+length_of_beam = int(input("What is the length of the beam? "))
+position_of_pivot = int(input("What is the position of the pivot? "))
+mass_of_beam = int(input("What is the mass of the beam? "))
+position_of_force = int(input("What is the position of the force"))
+print("")
