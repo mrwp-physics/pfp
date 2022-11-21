e0622c030e947724889dd14b4251b67436e114a7
NobodyNoteworthy <jessicanewton6@aol.com>
2022-11-21 13:31:13 +0000

    Added a velocity graph

--- jn-ball.py
+++ jn-ball.py
@@ -19,7 +19,7 @@ vx0, vy0 = 1, 0
 
 def get_pos(t=0):
     """A generator yielding the ball's position at time t."""
-    x, y, vx, vy = x0, y0, vx0, vy0
+    x, y, vy = x0, y0, vy0
     while x < XMAX:
         t += dt
         x += vx0 * dt
@@ -29,39 +29,63 @@ def get_pos(t=0):
             # bounce!
             y = 0
             vy = -vy * cor 
-        yield x, y
+        yield x, y,vy
+
+
+
 
 def init():
     """Initialize the animation figure."""
-    ax.set_xlim(0, XMAX)
-    ax.set_ylim(0, y0)
-    ax.set_xlabel('$x$ /m')
-    ax.set_ylabel('$y$ /m')
-    line.set_data(xdata, ydata)
-    ball.set_center((x0, y0))
+    #animation for s
+    ax[0].set_xlim(0, XMAX)
+    ax[0].set_ylim(0, y0)
+    ax[0].set_xlabel('$s(x)$ /m')
+    ax[0].set_ylabel('$s(y)$ /m')
+    line1.set_data(xdata, ydata)
+    ball1.set_center((x0, y0))
     height_text.set_text(f'Height: {y0:.1f} m')
-    return line, ball, height_text
+    #animation for v
+    ax[1].set_xlim(0, XMAX)
+    #calculation for y lim
+    tempy, vy = y0,vy0
+    while tempy>0:
+        tempy+=vy*dt
+        vy -= g*dt
+    ax[1].set_ylim(vy,-vy)
+    ax[1].set_xlabel('$s(x)$ /m')
+    ax[1].set_ylabel('$v(y)$ /m')
+    line2.set_data(xdata,vdata)
+    ball2.set_center((x0,vy0))
+
+
+    return line1, ball1, height_text, line2, ball2
 
 def animate(pos):
     """For each frame, advance the animation to the new position, pos."""
-    x, y = pos
+    x, y, v = pos
     xdata.append(x)
     ydata.append(y)
-    line.set_data(xdata, ydata)
-    ball.set_center((x, y))
+    vdata.append(v)
+    line1.set_data(xdata, ydata)
+    line2.set_data(xdata,vdata)
+    ball1.set_center((x, y))
+    ball2.set_center((x,v))
     height_text.set_text(f'Height: {y:.1f} m')
-    return line, ball, height_text
+    return line1, ball1, height_text,line2,ball2
 
 # Set up a new Figure, with equal aspect ratio so the ball appears round.
-fig, ax = plt.subplots()
-ax.set_aspect('equal')
+fig, ax = plt.subplots(2,constrained_layout = True)
+ax[0].set_aspect('equal')
 
 # These are the objects we need to keep track of.
-line, = ax.plot([], [], lw=2,color="red")
-ball = plt.Circle((x0, y0), 0.08,color="red")
-height_text = ax.text(XMAX*0.5, y0*0.8, f'Height: {y0:.1f} m')
-ax.add_patch(ball)
-xdata, ydata = [], []
+line1, = ax[0].plot([], [], lw=2,color="red")
+ball1 = plt.Circle((x0, y0), 0.08,color="red")
+height_text = ax[0].text(XMAX*0.5, y0*0.8, f'Height: {y0:.1f} m')
+ax[0].add_patch(ball1)
+line2, = ax[1].plot([],[],lw=2,color = "blue")
+ball2 = plt.Circle((x0,vy0),0.01,color="blue")
+ax[1].add_patch(ball2)
+xdata, ydata, vdata = [], [],[]
 
 interval = 1000*dt
 ani = animation.FuncAnimation(fig, animate, get_pos, blit=True,
